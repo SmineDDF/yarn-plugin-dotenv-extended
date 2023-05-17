24d8f6e18a01bc00b784ea3043378b2a04c5a14f
Kirill Lebedev <cyrill.lebedev@abbyy.com>
2023-05-17 18:48:49 +0200

    fix testcases for workspaces

--- test-workspaces/wp-1/test.js
+++ test-workspaces/wp-1/test.js
@@ -1,5 +1,5 @@
 const assert = require("assert").strict;
 const { env } = require("process");
 
-assert.equal(env.TEST_VARIABLE, "TEST_VALUE");
+assert.equal(env.TEST_VARIABLE, "WORKSPACE1_VALUE");
 assert.equal(env.DEFAULT_VARIABLE, 'DEFAULT_VALUE');
